c0450459302063c3320a085f5a355cf8a43863f0
Sean Klingler <sklingler@gmail.com>
2015-01-31 02:48:44 -0500

    another attempt at end of lesson 1

--- src/com/totalbeginner/newbie/Person.java
+++ src/com/totalbeginner/newbie/Person.java
@@ -8,5 +8,30 @@ package com.totalbeginner.newbie;
  *
  */
 public class Person {
+	private String name;
+	private int maxBooks;
 
+	/**
+	 * 
+	 */
+	public Person() {
+		name = "unknown person";
+		maxBooks = 3;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMaxBooks() {
+		return maxBooks;
+	}
+
+	public void setMaxBooks(int maxBooks) {
+		this.maxBooks = maxBooks;
+	}
 }
